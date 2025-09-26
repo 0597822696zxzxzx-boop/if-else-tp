d2151e0534a36839628f1f50bb5321793c95b1e8
qutaiba <0597822696zxzxzx@gmail.com>
2025-09-26 05:58:21 +0300

    first commit

--- practical_if_else_qutaiba.py
+++ practical_if_else_qutaiba.py
@@ -1,19 +1,10 @@
-name="qutaiba"
-last_name="almassri"
 firstname=input("enter their first name")
 lastname=input("enter thir last name")
-if firstname==name and last_name==lastname:
-    print("welcome",firstname,lastname)
-else:
-    print("error :both firstname and lastname must bbe privoded")
-cashedusername="ad"
-cashedpassword="ee2222" 
-username=input("pales enter name")
-password=input("palese enterpassword")
-if username== cashedusername and password== cashedpassword :
-    print("login")
-elif username== cashedusername and password!=cashedpassword :
-    print("no login")
-else:
-    print("error")
+if  not firstname and  not lastname:
+     print("error :both firstname and lastname no login")
 
+elif firstname and lastname:
+ print("welcome","firstname","lastname")
+
+else:
+ print("error")
